9e6e5ff56021314053c82ca868aba508f4ba9b21
xujinxing <xujinxing@zuoyebang.com>
2023-11-17 18:22:14 +0800

    name

--- src/pages/index/shangxi.vue
+++ src/pages/index/shangxi.vue
@@ -5,44 +5,54 @@
         <text v-for="label in state.labels" :key="label" :span="4" :class="{ active: label === state.label }" @click="labelChange(label)">{{ label }}</text>
       </section>
     </uni-card>
+    <section class="list-container">
+      <section class="list-item" v-for="item, index in state.list" :key="item.id" :class="['item-' + (index % 5 + 1)]">
+        <view class="badge">
+          <text class="square">{{ item.chuchu.charAt(0) }}</text>
+          <text class="triangle"></text>
+        </view>
+        <view class="chuchu">
+          {{ item.chuchu }}
+        </view>
+        <view class="text-content">
+          <mp-html :content="getMarkdownText(item.wenan)"></mp-html>
+        </view>
+        <view class="name-list">
+          <text v-for="name in getNameList(item.tuijian)" :key="name">#{{ name }}</text>
+        </view>
+      </section>
+    </section>
   </section>
 </template>
 <script lang="ts" setup>
 import request from '@/utils/request'
 import { reactive } from 'vue'
 import { logined } from '@/utils/init'
+import MpHtml from 'mp-html/dist/uni-app/components/mp-html/mp-html.vue'
+import { getMarkdownText } from './utils'
 
-const state = reactive({
+const state: any = reactive({
   label: '孤独',
-  labels: [
-    "孤独",
-    "伤感",
-    "浪漫",
-    "温暖",
-    "平静",
-    "唯美",
-    "爱慕",
-    "积极",
-    "怀念",
-    "坚定",
-    "自信",
-    "无奈",
-    "憧憬",
-    "深沉",
-    "乐观",
-    "自由",
-    "矛盾",
-    "活泼"
-  ]
+  labels: [],
+  list: []
 })
 const labelChange = (label: string) => {
   state.label = label
+  getList(label)
 }
-const getList = async () => {
+const getList = async (label: string = '') => {
   const data = await request({
     url: 'getShangxiList',
+    data: {
+      label
+    }
   })
-  console.log('data', data)
+  state.list = data.content || []
+  state.labels = data.label || []
+}
+const getNameList = (nameString: string) => {
+  const list = nameString.split('#')
+  return list.splice(0, 2).filter(name => name)
 }
 if (logined) {
   getList()
@@ -54,6 +64,9 @@ if (logined) {
 .conponent-container {
   height: 100%;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 
   uni-card {
     border-bottom-left-radius: 24px !important;
@@ -90,6 +103,165 @@ if (logined) {
       &.active {
         color: #5736FF;
         font-size: 14px;
+      }
+    }
+  }
+
+  .list-container {
+    padding: 6px 16px 24px 16px;
+    flex: 1;
+    overflow: auto;
+
+    .list-item {
+      margin-top: 10px;
+      border-radius: 10px;
+      padding: 8px;
+      position: relative;
+
+      .badge {
+        left: 0;
+        top: 0;
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+
+        .square {
+          width: 32px;
+          height: 23px;
+          background-color: transparent;
+          display: inline-block;
+          border-top-left-radius: 5px;
+          border-top-right-radius: 5px;
+          line-height: 28px;
+          text-align: center;
+          font-size: 14px;
+          color: white;
+          font-weight: 500;
+        }
+
+        .triangle {
+          display: inline-block;
+          width: 0;
+          height: 0;
+          border-left: 16px solid transparent;
+          border-right: 16px solid transparent;
+          border-top: 12px solid transparent;
+        }
+      }
+
+      .chuchu {
+        display: block;
+        height: 26px;
+        overflow: hidden;
+        padding-top: 0px;
+        box-sizing: border-box;
+        line-height: 26px;
+        padding-left: 32px;
+        font-size: 13px;
+        font-weight: 500;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+
+      .text-content {
+        overflow: hidden;
+        max-height: 48px;
+        line-height: 24px;
+        text-overflow: ellipsis;
+
+        mp-html {
+          font-size: 13px;
+          color: rgba(0, 0, 0, 0.7);
+        }
+
+        /* 控制显示的行数 */
+      }
+
+      .name-list {
+        margin-top: 2px;
+
+        text {
+          font-size: 12px;
+          font-weight: 500;
+          // color: whitel;
+          color: rgba(0, 0, 0, 0.75);
+          margin-right: 8px;
+        }
+      }
+
+      // #e5f3fe - #5781fb 
+      // #fbf1ef - #ff8671
+      // #dbf4f0 - #37c6b0
+      // #e3e5fe - #9f88fd
+      // #fff5ec - #ffab6b
+      &.item-1 {
+        background-color: #e5f3fe;
+
+        .badge {
+          .square {
+            background-color: #5781fb;
+          }
+
+          .triangle {
+            border-top-color: #5781fb;
+          }
+        }
+      }
+
+      &.item-2 {
+        background-color: #fbf1ef;
+
+        .badge {
+          .square {
+            background-color: #ff8671;
+          }
+
+          .triangle {
+            border-top-color: #ff8671;
+          }
+        }
+      }
+
+      &.item-3 {
+        background-color: #dbf4f0;
+
+        .badge {
+          .square {
+            background-color: #37c6b0;
+          }
+
+          .triangle {
+            border-top-color: #37c6b0;
+          }
+        }
+      }
+
+      &.item-4 {
+        background-color: #e3e5fe;
+
+        .badge {
+          .square {
+            background-color: #9f88fd;
+          }
+
+          .triangle {
+            border-top-color: #9f88fd;
+          }
+        }
+      }
+
+      &.item-5 {
+        background-color: #fff5ec;
+
+        .badge {
+          .square {
+            background-color: #ffab6b;
+          }
+
+          .triangle {
+            border-top-color: #ffab6b;
+          }
+        }
       }
     }
   }
